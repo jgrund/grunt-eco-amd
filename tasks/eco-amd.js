885b0b95e8aa2e223c3ad804907c72e9311381a7
Joe Grund <joe@oneluxsystems.com>
2012-11-24 12:03:08 -0500

    Cleaned up code.

--- tasks/eco-amd.js
+++ tasks/eco-amd.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict';
 
   // TODO: ditch this when grunt v0.4 is released
@@ -16,43 +16,46 @@ module.exports = function(grunt) {
     this.requiresConfig('eco_amd');
 
     var path = require('path');
-
     var helpers = require('grunt-contrib-lib').init(grunt);
-
-    var basePath;
-
-    var srcFiles;
-
-    var newFileDest;
 
     var options = helpers.options(this, {
       basePath: false
     });
 
+    var basePath;
+    var srcFiles;
+    var newFileDest;
+
     // TODO: ditch this when grunt v0.4 is released
     this.files = this.files || helpers.normalizeMultiTaskFiles(this.data, this.target);
 
-    this.files.forEach(function (file) {
-        file.dest = path.normalize(file.dest);
-        srcFiles = grunt.file.expandFiles(file.src);
+    /**
+     * Compiles an eco template with an AMD compatible function wrapper.
+     * @param {String} srcFile The file to read in.
+     * @param {Function} callback Function to call with the compiled template as an argument.
+     * @return {undefined}
+     */
+    function compileEco(srcFile, callback) {
+      var indent = require('eco/lib/util').indent;
+      var source = grunt.file.read(srcFile);
+      var compiled = 'define(function () {\nreturn ' + indent(require('eco').precompile(source), 2).slice(2) + ' \n});\n';
 
-        basePath = helpers.findBasePath(srcFiles, options.basePath);
-        
-        srcFiles.forEach(function (srcFile) {
-          newFileDest = helpers.buildIndividualDest(file.dest, srcFile, basePath)
-          
-          compileEco(srcFile, function (compiled) {
-            grunt.file.write(newFileDest, compiled)
-          }); 
+      callback(compiled);
+    }
+
+    this.files.forEach(function (file) {
+      file.dest = path.normalize(file.dest);
+      srcFiles = grunt.file.expandFiles(file.src);
+
+      basePath = helpers.findBasePath(srcFiles, options.basePath);
+
+      srcFiles.forEach(function (srcFile) {
+        newFileDest = helpers.buildIndividualDest(file.dest, srcFile, basePath);
+
+        compileEco(srcFile, function (compiled) {
+          grunt.file.write(newFileDest, compiled);
         });
+      });
     });
   });
-
-  var compileEco = function(srcFile, callback) {
-    var indent = require('eco/lib/util').indent;
-    var source = grunt.file.read(srcFile);
-    var compiled = 'define(function () {\nreturn ' + indent(require('eco').precompile(source), 2).slice(2) + ' \n});\n';
-    
-    callback(compiled);
-  };
 };
